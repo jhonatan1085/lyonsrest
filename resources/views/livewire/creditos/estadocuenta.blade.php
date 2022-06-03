e185f77a5eb317514ef03e38bad841e0341a71f6
jhonatan1085 <jhonatan1085@gmail.com>
2022-06-02 21:58:10 -0500

    estado de cuenta 1

--- resources/views/livewire/creditos/estadocuenta.blade.php
+++ resources/views/livewire/creditos/estadocuenta.blade.php
@@ -15,7 +15,7 @@
     <div class="clearfix"></div>
 
     <div class="col-md-4 col-sm-4  profile_details">
-      <div class="well profile_view">
+      <div class="well profile_view col-sm-12">
         <div class="col-sm-12">
           <h4 class="brief"><i>Datos de Cliente</i></h4>
           <div class="left col-md-12 col-sm-12">
@@ -41,6 +41,38 @@
         </div>
       </div>
     </div>
+    <div class="col-md-8 col-sm-8  profile_details">
+        <div class="well profile_view">
+            <ul class="nav nav-tabs bar_tabs" id="myTab" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+              </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+              <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher
+                    synth. Cosby sweater eu banh mi, qui irure terr.
+              </div>
+              <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo
+                    booth letterpress, commodo enim craft beer mlkshk aliquip
+              </div>
+              <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo
+                    booth letterpress, commodo enim craft beer mlkshk 
+              </div>
+            </div>
+        </div>
+        
+      </div>
+
+
         @include('livewire.creditos.modal')
 
 
